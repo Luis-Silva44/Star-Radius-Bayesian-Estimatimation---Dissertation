c2eef45b6c8128945b1b7959930265188872fca1
Luis-Silva44 <luismms1999@hotmail.com>
2025-01-05 19:01:17 +0000

    Module to do SED fitting and get radius

--- SED_fitting.py
+++ SED_fitting.py
@@ -1,12 +1,14 @@
-# %% 
+# %% Imports
 from gaia_module import gaia_values
 from wise_module import * 
 from two_mass_module import * 
 from SED_flux import *
+from graphs_visualization import * 
 
-import uncertainties as u
+import astropy.units as u
+from astropy.constants import R_sun
 import numpy as np
-
+from scipy.optimize import minimize
 # %% Function that allows easy unit change
 
 def flux_unit_change(value,unit):
@@ -31,10 +33,61 @@ def get_flux_values(star_name):
     two_mass_flux = two_mass_values(star_name)
     wise_flux = wise_values(star_name)
 
-    flux_values = np.concatenate(gaia_flux, two_mass_flux, wise_flux)
-    return flux_values
+    flux_values = np.concatenate((gaia_flux, two_mass_flux, wise_flux))
+    filter_wavelen = band_wavelen()
+
+    flux_values_Jy = flux_values.to(u.Jy, equivalencies=u.spectral_density(filter_wavelen))
+    return flux_values_Jy
+
+# %%
+
+def SED_fitting(star_name, Teff, mettalicity, log_g, Ebv, unit):
+    _, parallax = gaia_values(star_name)
+    unit_change = 1 * u.parsec
+    distance = (1 / parallax.value) * unit_change
+    photometry_flux_Jy = get_flux_values(star_name)
+    wavelen, SED_flux_Jy = SED_flux_bands(Teff, mettalicity, log_g, Ebv)
+    
+    photometry_flux = flux_unit_change(photometry_flux_Jy, unit)
+    SED_flux = flux_unit_change(SED_flux_Jy, unit)
+
+    photometry_flux_vals = []
+    photometry_flux_unc = []
+    for i in range(len(wavelen)):
+        photometry_flux_vals.append(photometry_flux[i].value.nominal_value)
+        photometry_flux_unc.append(photometry_flux[i].value.std_dev)
+
+    photometry_flux_vals = np.array(photometry_flux_vals)
+    photometry_flux_unc = np.array(photometry_flux_unc) 
+
+
+    def minimization_function(radius, distance, SED_flux, photometry_flux_vals, photometry_flux_unc):
+        SED_flux = SED_flux * radius**2 / (distance.value.nominal_value * u.parsec).to(R_sun) ** 2
+        chi_squared = np.sum(((SED_flux.value - photometry_flux_vals)/ photometry_flux_unc) ** 2)
+        return chi_squared
+    
+    minimization_result = minimize(minimization_function, x0=1.0, args=(distance,SED_flux,photometry_flux_vals, photometry_flux_unc), method='Nelder-Mead')
+    minimization_radius= minimization_result.x[0]
+
+    minimization_radius = (minimization_radius * R_sun).to(R_sun)
+
+    SED_wavelen, SED_att = SED_attenuated(Teff,mettalicity,log_g,Ebv)
+    SED_att = flux_unit_change(SED_att, unit)
+    SED_fitting_plot(SED_wavelen, SED_att, photometry_flux_vals, photometry_flux_unc, minimization_radius.value, distance)
+
+    return minimization_radius, minimization_result
+
 # %% 
 
-#retrieve_gaia_id(704967037090946688)
+star_name = 'WASP-84'	
+Teff = 5221 
+logg = 4.28
+mettalicity = 0.05
+table_value = (0.828 * R_sun).to(R_sun)
+Ebv = 0.020
+#distance = 100.4
 
-two_mass_values(704967037090946688)
+radius, _ = SED_fitting(star_name, Teff, mettalicity, logg, Ebv, 'Jy')
+error = abs(radius - table_value) / table_value * 100
+print(radius,error)
+# %%
