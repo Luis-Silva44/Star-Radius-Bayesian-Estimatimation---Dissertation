bee81d0fb7993eea38558a806e0083e2136cbdb6
Luis <luismms1999@hotmail.com>
2025-01-12 20:50:00 +0000

    Added a check in case WISE bands are not found

--- wise_module.py
+++ wise_module.py
@@ -4,19 +4,20 @@ import astropy.units as u
 from astroquery.vizier import Vizier
 from astropy.coordinates import SkyCoord
 from uncertainties import ufloat
+import math
 
 from auxiliary_functions import * 
 
 # %% 
 # Function to get the photometry values and errors and turn them into fluxes
 def wise_values(star_name):
+    Vizier.ROW_LIMIT = 1
     wise_catalog = 'II/311/wise'
     ra, dec = vizier_coords(star_name)
     coords = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs')
-
-    wise_data = Vizier.query_region(coords, radius=10* u.arcsec , catalog=wise_catalog)
-
-    if wise_values:
+    wise_data = Vizier.query_region(coords, radius=15* u.arcsec , catalog=wise_catalog)
+    
+    if wise_data:
         W1_mag = ufloat(wise_data[0]['W1mag'], wise_data[0]['e_W1mag'])
         W2_mag = ufloat(wise_data[0]['W2mag'], wise_data[0]['e_W2mag'])
         W3_mag = ufloat(wise_data[0]['W3mag'], wise_data[0]['e_W3mag'])
@@ -27,13 +28,24 @@ def wise_values(star_name):
         W3_flux= mag_to_flux(W3_mag,'W3')
         W4_flux= mag_to_flux(W4_mag,'W4')
 
-        unit = 1 * u.watt / u.um / u.cm**2
-        wise_flux = np.array([W1_flux, W2_flux, W3_flux, W4_flux]) * unit
+        if math.isnan(W4_mag.std_dev) == True:
 
-        return wise_flux
+            unit = 1 * u.watt / u.um / u.cm**2
+            wise_flux = np.array([W1_flux, W2_flux, W3_flux]) * unit
+            wise_check = ['W1','W2','W3']
+        else: 
+            unit = 1 * u.watt / u.um / u.cm**2
+            wise_flux = np.array([W1_flux, W2_flux, W3_flux, W4_flux]) * unit
+            wise_check = ['W1','W2','W3','W4']
+        
     else:
-        raise ValueError('No WISE data found')
+        unit = 1 * u.watt / u.um / u.cm**2
+        wise_check = []
+        wise_flux = [] * unit
+        print('No WISE data found')
     
+    return wise_flux, wise_check
 # %%
 
-#wise_values('55 Cnc')
+wise_values('WASP-8')
+
