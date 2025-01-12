7b3a8b0a1d00e2387e90122cc58db84a83dad930
Luis <luismms1999@hotmail.com>
2025-01-12 20:49:20 +0000

    Added a check in case no gaia bands are found

--- gaia_module.py
+++ gaia_module.py
@@ -27,8 +27,10 @@ def gaia_values(star_name):
         GRP_flux = GRP_flux * 1.638483E-21 
         gaia_flux = np.array([GBP_flux, G_flux, GRP_flux]) * unit
 
+        gaia_check = ['GBP', 'G', 'GRP']
+
         unit = 1 * u.mas
-        return gaia_flux.to(u.watt / u.um / u.cm**2), (gaia_parallax * unit).to(u.arcsec)
+        return gaia_flux.to(u.watt / u.um / u.cm**2), (gaia_parallax * unit).to(u.arcsec), gaia_check
 
     else:
         raise ValueError('No Gaia ID found')
