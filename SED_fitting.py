396576f22a6ca0d04a79c1f2d755b82c0a556f42
Luis <luismms1999@hotmail.com>
2025-01-12 20:47:21 +0000

    Troubleshooting problem stars

--- SED_fitting.py
+++ SED_fitting.py
@@ -1,37 +1,38 @@
 # %% Imports
 from auxiliary_functions import *
-from gaia_module import gaia_values
+from gaia_module import *
 from wise_module import * 
 from two_mass_module import * 
 from SED_flux import *
 from graphs_visualization import * 
 
 import astropy.units as u
+import time
 from astropy.constants import R_sun
 import numpy as np
 from scipy.optimize import minimize
-import time
 
 # %% 
 def get_flux_values(star_name):
-    gaia_flux, _ = gaia_values(star_name)
-    two_mass_flux = two_mass_values(star_name)
-    wise_flux = wise_values(star_name)
+    gaia_flux, _, gaia_check = gaia_values(star_name)
+    two_mass_flux, two_mass_check= two_mass_values(star_name)
+    wise_flux, wise_check = wise_values(star_name)
 
     flux_values = np.concatenate((gaia_flux, two_mass_flux, wise_flux))
-    filter_wavelen = band_wavelen()
 
+    filter_check = gaia_check + two_mass_check + wise_check
+    filter_wavelen = band_wavelen(filter_check)
     flux_values_Jy = flux_values.to(u.Jy, equivalencies=u.spectral_density(filter_wavelen))
-    return flux_values_Jy
+    return filter_wavelen, flux_values_Jy
 
 # %%
 
 def SED_fitting(star_name, Teff, mettalicity, log_g, Ebv, unit):
-    _, parallax = gaia_values(star_name)
+    _, parallax, _ = gaia_values(star_name)
     unit_change = 1 * u.parsec
     distance = (1 / parallax.value) * unit_change
-    photometry_flux_Jy = get_flux_values(star_name)
-    wavelen, SED_flux_Jy = SED_flux_bands(Teff, mettalicity, log_g, Ebv)
+    filter_wavelen, photometry_flux_Jy = get_flux_values(star_name)
+    wavelen, SED_flux_Jy = SED_flux_bands(filter_wavelen, Teff, mettalicity, log_g, Ebv)
     
     photometry_flux = flux_unit_change(photometry_flux_Jy, unit)
     SED_flux = flux_unit_change(SED_flux_Jy, unit)
@@ -45,7 +46,6 @@ def SED_fitting(star_name, Teff, mettalicity, log_g, Ebv, unit):
     photometry_flux_vals = np.array(photometry_flux_vals)
     photometry_flux_unc = np.array(photometry_flux_unc) 
 
-
     def minimization_function(radius, distance, SED_flux, photometry_flux_vals, photometry_flux_unc):
         SED_flux = SED_flux * radius**2 / (distance.value.nominal_value * u.parsec).to(R_sun) ** 2
         chi_squared = np.sum(((SED_flux.value - photometry_flux_vals)/ photometry_flux_unc) ** 2)
@@ -58,35 +58,93 @@ def SED_fitting(star_name, Teff, mettalicity, log_g, Ebv, unit):
 
     SED_wavelen, SED_att = SED_attenuated(Teff,mettalicity,log_g,Ebv)
     SED_att = flux_unit_change(SED_att, unit)
-    SED_fitting_plot(SED_wavelen, SED_att, photometry_flux_vals, photometry_flux_unc, minimization_radius.value, distance)
+    SED_fitting_plot(SED_wavelen, SED_att, filter_wavelen, photometry_flux_vals, photometry_flux_unc, minimization_radius.value, distance)
 
     return minimization_radius, minimization_result
 
-# %% Test cell
-
-star_name = 'WASP-84'	
-Teff = 5221 
-logg = 4.28
-mettalicity = 0.05
-table_value = (0.828 * R_sun).to(R_sun)
-Ebv = 0.020
-
-#radius, _ = SED_fitting(star_name, Teff, mettalicity, logg, Ebv, 'Jy')
-#error = abs(radius - table_value) / table_value * 100
-#print('Computed radius is:', radius)
-#print('Table value of radius:', table_value)
-#print('Error:', error, '%')
+# %% 
+def single_star_tester(star_name, Teff, mettalicity, logg, Ebv, table_value, unit):
+    radius, _ = SED_fitting(star_name, Teff, mettalicity, logg, Ebv, unit)
+    error = abs(radius - table_value) / table_value * 100
+    print('Computed radius is:', radius)
+    print('Table value of radius:', table_value)
+    print('Error:', error, '%')
+    return radius
 
 # %% 
+def star_set_tester(star_list, unit):
+    time_start = time.time()
+
+    problem_stars = []
+    computed_radius = []
+    table_value_radius = []
+
+    for i in range(len(star_list)):
+        star_name = star_list['Star'][i]
+        Teff = star_list['Teff'][i]
+        mettalicity = star_list['Fe/H'][i]
+        log_g = star_list['logg'][i]
+        Ebv = float(star_list['E(B-V)'][i])
+        table_value = star_list['Radius'][i]
+
+        table_value = table_value * R_sun
+        table_value = table_value.to(R_sun)
+
+        print('Star being tested:', star_name)
+
+        try:
+            radius, _ = SED_fitting(star_name, Teff, mettalicity, log_g, Ebv, unit)
+            computed_radius.append(radius)
+            table_value_radius.append(table_value)
+                
+            print('Value of radius computed:', radius)
+            print('Table value of radius:', table_value)
+            print('Error in value computed:', abs(table_value - radius) / table_value * 100)
+            print('--------')
+
+        except Exception as e:
+            problem_stars.append(star_name)
+            print('Issue with star', star_name)
+            print('--------')
+
+    time_end = time.time()
+    print('Program took', time_end - time_start, 'seconds to run for', len(star_list),'stars')
+    print(len(problem_stars), 'stars had issues with computing radius')
+
+    return problem_stars, computed_radius, table_value_radius
+
+# %%
 
 star_data = pd.read_csv('~/tese/testdata/list_stars.txt', sep="\t", header=0, skiprows=[1])
 star_test_subset = star_data.head()
 
 # Uncomment to run for list of stars (approx. 45 seconds)
-# problem_list, computed_list, table_list = star_set_mean_tester(star_data, 'SI')
+problem_list, computed_list, table_list = star_set_tester(star_data, 'SI')
 
 # %% 
 computed_radii = np.array([i.value for i in computed_list])
 table_values = np.array([i.value for i in table_list])
 
 computed_real_comparison(computed_radii, table_values)
+
+# %%
+print(problem_list)
+
+'''['GJ176', # temperature too low, outside of model range
+ 'TIC209464063', # two mass module finds wrong star; nan values in uncertainties of two mass bands
+ 'TOI-1797', # two mass module finds wrong star; nan values in uncertainties of two mass bands
+ 'TOI-2350', # no simbad entry, need to adapt for inputation of direct coordinates
+ 'TOI-4629', # no simbad entry, need to adapt for inputation of direct coordinates
+ 'TOI-5493', # no simbad entry, need to adapt for inputation of direct coordinates
+ 'TOI-5696', # no simbad entry, need to adapt for inputation of direct coordinates
+ 'WASP-8'] # programm is finding the wrong star in vizier search
+''' 
+# %% 
+star_name = 'TOI-815'
+Teff = 4869
+logg = 4.51
+mettalicity = -0.09
+table_value = (0.77 * R_sun).to(R_sun)
+Ebv = 0.015
+#single_star_tester(star_name, Teff, mettalicity, logg, Ebv, table_value, 'SI')
+# %%
