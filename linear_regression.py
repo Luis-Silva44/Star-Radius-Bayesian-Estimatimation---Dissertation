f4c2f3cd9971c8b75fc5bb53118ca70df65dc7ab
Luis <luismms1999@hotmail.com>
2025-02-03 12:05:50 +0000

    Testing different effects of priors and initial settings

--- linear_regression.py
+++ linear_regression.py
@@ -6,7 +6,7 @@ import corner
 from IPython.display import display, Math
 # %%
 
-np.random.seed(123)
+np.random.seed(82)
 
 # Choose the "true" parameters.
 m_true = -0.9594
@@ -15,9 +15,9 @@ b_true = 4.294
 # Generate some synthetic data from the model.
 N = 50
 x = np.sort(10 * np.random.rand(N))
-yerr = 0.1 + 0.5 * np.random.rand(N)
+yerr = 0.1 * np.random.rand(N)
 y = m_true * x + b_true
-y += yerr * np.random.randn(N)
+y += yerr 
 
 plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
 x0 = np.linspace(0, 10, 500)
