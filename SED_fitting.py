0ac579eb1530dc74fca4d46ee5dcf7ecb970aeaa
Luis <luismms1999@hotmail.com>
2025-02-05 17:37:35 +0000

    Small tweaks

--- SED_fitting.py
+++ SED_fitting.py
@@ -32,20 +32,20 @@ def SED_fitting(star_name, Teff, mettalicity, log_g, Ebv, unit):
     unit_change = 1 * u.parsec
     distance = (1 / parallax.value) * unit_change
     filter_wavelen, photometry_flux_Jy = get_flux_values(star_name)
-    wavelen, SED_flux_Jy = SED_flux_bands(filter_wavelen, Teff, mettalicity, log_g, Ebv)
+    _, SED_flux_Jy = SED_flux_bands(filter_wavelen, Teff, mettalicity, log_g, Ebv)
     
     photometry_flux = flux_unit_change(photometry_flux_Jy, unit)
     SED_flux = flux_unit_change(SED_flux_Jy, unit)
 
     photometry_flux_vals = []
     photometry_flux_unc = []
-    for i in range(len(wavelen)):
+    for i in range(len(filter_wavelen)):
         photometry_flux_vals.append(photometry_flux[i].value.nominal_value)
         photometry_flux_unc.append(photometry_flux[i].value.std_dev)
 
     photometry_flux_vals = np.array(photometry_flux_vals)
     photometry_flux_unc = np.array(photometry_flux_unc) 
-
+    
     def minimization_function(radius, distance, SED_flux, photometry_flux_vals, photometry_flux_unc):
         SED_flux = SED_flux * radius**2 / (distance.value.nominal_value * u.parsec).to(R_sun) ** 2
         chi_squared = np.sum(((SED_flux.value - photometry_flux_vals)/ photometry_flux_unc) ** 2)
@@ -140,10 +140,12 @@ HD 219134
 HD128582
 '''
 # %% 
-star_name = 'HD 209458'
-Teff = 5353
-logg = 4.3
-mettalicity = 0.3
-table_value = (0.941 * R_sun).to(R_sun)
-Ebv = 0.043
-#single_star_tester(star_name, Teff, mettalicity, logg, Ebv, table_value, 'SI')
+star_name ='WASP-84'
+Teff = 5221
+logg = 4.28
+mettalicity = 0.05
+table_value = (0.828 * R_sun).to(R_sun)
+Ebv = 0.074
+single_star_tester(star_name, Teff, mettalicity, logg, Ebv, table_value, 'SI')
+
+# %%
