2f5df2a081097ca9d2723a3979659802694cb898
Luis <luismms1999@hotmail.com>
2025-02-19 16:30:59 +0000

    Testing stars

--- MCMC_extinction.py
+++ MCMC_extinction.py
@@ -18,9 +18,17 @@ from scipy.stats import norm
 
 # %%
 
-def likelihood(params, obs_flux, obs_flux_unc, model_flux, filter_wavelen):
-        distance, Ebv, radius = params
+def likelihood(params, obs_flux, obs_flux_unc, log_g, metallicity, filter_wavelen):
+        distance, temperature, Ebv, radius = params
         distance2 = (distance * u.pc).to(R_sun)
+        SED_wavelen, model_flux = SED_interpolator(temperature, metallicity, log_g)
+
+        nearest_index = []
+        for i in range(len(filter_wavelen)):
+            nearest_index.append(find_nearest_index(SED_wavelen, filter_wavelen[i]))
+
+        model_flux = np.array([model_flux[i].value for i in nearest_index]) 
+
         filter_wavelen_ = filter_wavelen.astype(np.float64)
         flux_attenuated = flux_extinction(filter_wavelen_, model_flux, Ebv)
         model_flux_scaled = flux_attenuated * (radius / distance2.value)**2
@@ -28,24 +36,25 @@ def likelihood(params, obs_flux, obs_flux_unc, model_flux, filter_wavelen):
         c = np.log(2 * np.pi * obs_flux_unc**2)
         return -0.5 * np.sum(c + ((obs_flux - model_flux_scaled)**2 / obs_flux_unc**2))
 
-def prior(params, exp_distance):
-        distance, Ebv, radius = params
+def prior(params, exp_distance, exp_temperature, temp_unc):
+        distance, temperature, Ebv, radius = params
 
-        if not (0.1 < radius < 10.0) or not (0.0 < Ebv < 0.5):
+        if not (0.1 < radius < 10.0) or not (0.0 < Ebv < 0.5) or not(3500 < temperature < 10000):
             return -np.inf
         
         distance_prior = -0.5 * ((distance - exp_distance.value.nominal_value) / exp_distance.value.std_dev)**2
-        return distance_prior
+        temperature_prior = -0.5*((temperature - exp_temperature) / temp_unc)**2
+        return distance_prior + temperature_prior
 
-def posterior(params, obs_flux, obs_flux_unc, model_flux, filter_wavelen, exp_distance):
-        log_prior = prior(params, exp_distance)
+def posterior(params, obs_flux, obs_flux_unc, exp_distance, exp_temperature, temp_unc, filter_wavelen):
+        log_prior = prior(params, exp_distance, exp_temperature, temp_unc)
         if not np.isfinite(log_prior):
             return -np.inf
     
-        return log_prior + likelihood(params, obs_flux, obs_flux_unc, model_flux, filter_wavelen)
+        return log_prior + likelihood(params, obs_flux, obs_flux_unc, log_g, metallicity, filter_wavelen)
 # %% 
-def basic_MCMC(exp_values, nwalkers, obs_flux, obs_flux_unc, model_flux, filter_wavelen):
-    exp_distance, expected_Ebv, expected_radius = exp_values 
+def basic_MCMC(exp_values, nwalkers, obs_flux, obs_flux_unc, log_g, metallicity, filter_wavelen):
+    exp_distance, exp_temperature, temp_unc, exp_Ebv, exp_radius = exp_values 
 
     pos = np.array([exp_distance.value.nominal_value + exp_distance.value.std_dev * np.random.randn(nwalkers), 
                 np.abs(np.random.normal(0.5, 0.2, size=nwalkers)),
@@ -53,12 +62,12 @@ def basic_MCMC(exp_values, nwalkers, obs_flux, obs_flux_unc, model_flux, filter_
 
     nwalkers, ndim = pos.shape
 
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, posterior, args=(obs_flux, obs_flux_unc, model_flux, filter_wavelen, exp_distance), pool = multiprocessing.Pool(16))
-    state = sampler.run_mcmc(pos, 2500, progress=True)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, posterior, args=(obs_flux, obs_flux_unc, exp_distance, exp_temperature, temp_unc, exp_Ebv, log_g, metallicity, filter_wavelen), pool = multiprocessing.Pool(16))
+    state = sampler.run_mcmc(pos, 1500, progress=True)
 
-    labels = ["Distance", "E(B-V)", "Radius"]
+    labels = ["Distance", "Temperature", "E(B-V)", "Radius"]
     fig, axes = plt.subplots(3, figsize=(10, 7), sharex=True)
-    samples = sampler.get_chain(discard=1250)
+    samples = sampler.get_chain(discard=500)
 
     for i in range(ndim):
         ax = axes[i]
@@ -73,7 +82,7 @@ def basic_MCMC(exp_values, nwalkers, obs_flux, obs_flux_unc, model_flux, filter_
     fig = corner.corner(flat_samples, labels=labels)
     plt.show()
 
-    expected_values = [exp_distance.value.nominal_value, expected_Ebv, expected_radius.value]
+    expected_values = [exp_distance.value.nominal_value, exp_temperature, exp_Ebv, exp_radius.value]
     for i in range(ndim):
         mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
         q = np.diff(mcmc)
@@ -82,7 +91,7 @@ def basic_MCMC(exp_values, nwalkers, obs_flux, obs_flux_unc, model_flux, filter_
         display(Math(txt))
         print('Error in ', labels[i], '=', abs(mcmc[1] - expected_values[i]) / expected_values[i] * 100)
         if i == 2:
-            return (mcmc[1] * R_sun).to(R_sun)
+            return (mcmc[1] * R_sun).to(R_sun), sampler
 
 # %%
 def star_set_tester_MCMC(star_list):
@@ -141,7 +150,7 @@ star_data = pd.read_csv('~/tese/testdata/list_stars.txt', sep="\t", header=0, sk
 star_test_subset = star_data.head()
 
 # %%
-problem_stars, computed_list, table_list = star_set_tester_MCMC(star_data)
+#problem_stars, computed_list, table_list = star_set_tester_MCMC(star_data)
 # %%
 computed_radii = np.array([i.value for i in computed_list])
 table_values = np.array([i.value for i in table_list])
@@ -174,5 +183,7 @@ model_flux = np.array([SED_flux[i].value for i in nearest_index])
 exp_values = (exp_distance, expected_Ebv, table_value)
 
 basic_MCMC(exp_values, 10, obs_flux, obs_flux_unc, model_flux, filter_wavelen)
+
+# %%
 
 # %%
