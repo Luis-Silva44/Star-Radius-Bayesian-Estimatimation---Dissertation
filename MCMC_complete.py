1e6d50a9b612efa76bd9bb2a65f256a5aaf9937b
Luis <luismms1999@hotmail.com>
2025-03-10 11:29:40 +0000

    Routine to compute radius using MCMC and all parameters

--- MCMC_complete.py
+++ MCMC_complete.py
@@ -12,36 +12,48 @@ import emcee as emcee
 import multiprocessing
 import corner
 from IPython.display import display, Math
-import time
+import scipy.stats as stats
+
 # %%
 def likelihood(params, obs_flux, obs_flux_unc, filter_wavelen):
         distance, temperature, log_g, metallicity, Ebv, radius = params
         distance2 = (distance * u.pc).to(R_sun)
+
         SED_wavelen, model_flux = SED_interpolator(temperature, metallicity, log_g)
-        nearest_index = []
-        for i in range(len(filter_wavelen)):
-            nearest_index.append(find_nearest_index(SED_wavelen, filter_wavelen[i]))
+        
+        filter_bands = {'GBP':0.532, 'G': 0.673, 'GRP':0.797, 
+                    'J':1.25, 'H':1.65, 'K':2.15,
+                    'W1':3.4, 'W2':4.6, 'W3':12, 'W4':22} 
+    
+        selected_bands = [name for name, wav in filter_bands.items() if wav in filter_wavelen.value]
+        SED_values = []
+        for i in selected_bands:
+            SED_values.append(convolution(model_flux, SED_wavelen, i))
 
-        model_flux = np.array([model_flux[i].value for i in nearest_index]) 
+        model_flux = np.array(SED_values)
+        filter_wavelen = filter_wavelen.astype(np.float64)
+        flux_attenuated = flux_extinction(filter_wavelen, model_flux, Ebv)
 
-        filter_wavelen_ = filter_wavelen.astype(np.float64)
-        flux_attenuated = flux_extinction(filter_wavelen_, model_flux, Ebv)
         model_flux_scaled = flux_attenuated * (radius / distance2.value)**2
 
-        c = np.log(2 * np.pi * obs_flux_unc**2)
-        return -0.5 * np.sum(c + ((obs_flux - model_flux_scaled)**2 / obs_flux_unc**2))
+        #c = np.log(2 * np.pi * obs_flux_unc**2)
+        
+        #return (-0.5 * np.sum(c + ((obs_flux - model_flux_scaled)**2 / obs_flux_unc**2)))
+    
+        return np.sum(stats.norm.logpdf(obs_flux, loc = model_flux_scaled, scale = obs_flux_unc))
 
 def prior(params, exp_distance, exp_temperature, temp_unc, exp_log, log_unc, exp_met, met_unc):
-        distance, temperature, log_g, metallicty, Ebv, radius = params
+        distance, temperature, log_g, metallicity, Ebv, radius = params
 
-        if not (0.1 < radius < 10.0) or not (0.0 < Ebv < 0.5) or not(3500 < temperature < 10000) or not (-2.5 < metallicty < 0.5) or not (0 < log_g < 5.0):
+        if not (0.1 < radius < 10.0) or not(3500 < temperature < 10000) or not (-2.5 < metallicity < 0.5) or not (0 < log_g < 5.0):
             return -np.inf
         
-        distance_prior = -0.5 * ((distance - exp_distance.value.nominal_value) / exp_distance.value.std_dev)**2
-        temperature_prior = -0.5*((temperature - exp_temperature) / temp_unc)**2
-        log_g_prior = -0.5 *((log_g - exp_log) / log_unc)**2
-        met_prior = -0.5 * ((metallicty - exp_met) / met_unc)**2
-        return distance_prior + temperature_prior + log_g_prior + met_prior
+        distance_prior = stats.norm.logpdf(distance, loc = exp_distance.value.nominal_value, scale = exp_distance.value.std_dev)
+        temperature_prior = stats.norm.logpdf(temperature, loc = exp_temperature, scale = temp_unc)
+        log_g_prior = stats.norm.logpdf(log_g, loc = exp_log, scale = log_unc)
+        met_prior = stats.norm.logpdf(metallicity, loc = exp_met, scale = met_unc)
+        
+        return (distance_prior + temperature_prior + log_g_prior + met_prior) 
 
 def posterior(params, obs_flux, obs_flux_unc, exp_distance, exp_temperature, temp_unc, exp_log, log_unc, exp_met, met_unc, filter_wavelen):
         log_prior = prior(params, exp_distance, exp_temperature, temp_unc, exp_log, log_unc, exp_met, met_unc)
@@ -64,7 +76,7 @@ def complete_MCMC(exp_values, nwalkers, obs_flux, obs_flux_unc, filter_wavelen):
     nwalkers, ndim = pos.shape
 
     sampler = emcee.EnsembleSampler(nwalkers, ndim, posterior, args=(obs_flux, obs_flux_unc, exp_distance, exp_temperature, temp_unc, exp_log, log_unc, exp_met, met_unc, filter_wavelen), pool = multiprocessing.Pool(12))
-    state = sampler.run_mcmc(pos, 1500, progress=True)
+    state = sampler.run_mcmc(pos, 200, progress=True)
 
     labels = ["Distance", "Temperature", "Log g", "Metallicity", "E(B-V)", "Radius"]
     fig, axes = plt.subplots(6, figsize=(10, 7), sharex=True)
@@ -79,7 +91,7 @@ def complete_MCMC(exp_values, nwalkers, obs_flux, obs_flux_unc, filter_wavelen):
 
     axes[-1].set_xlabel("step number")
 
-    flat_samples = sampler.get_chain(discard = 500, flat=True)
+    flat_samples = sampler.get_chain(flat=True)
     fig = corner.corner(flat_samples, labels=labels)
     plt.show()
 
@@ -163,10 +175,40 @@ filter_wavelen, flux_values = get_flux_values(star_name)
 obs_flux = np.array([m.value.nominal_value for m in flux_values])
 obs_flux_unc = np.array([m.value.std_dev for m in flux_values])
 
-
 exp_values = (exp_distance, exp_Teff, Teff_unc, log_g, log_unc, metallicity, met_unc, expected_Ebv, table_value)
 
 _, sampler3 = complete_MCMC(exp_values, 12, obs_flux, obs_flux_unc, filter_wavelen)
 
 
+# %% 
+temp_values = np.linspace(5100, 6900, 20)
+
+prior_list = []
+likelihood_list = []
+posterior_list = []
+
+for i in temp_values:
+    params = (exp_distance.value.nominal_value, i, log_g, metallicity, expected_Ebv, table_value.value)
+    prior_value = prior(params, exp_distance, exp_Teff, Teff_unc, log_g, log_unc, metallicity, met_unc)
+    likelihood_value = likelihood(params, obs_flux, obs_flux_unc, filter_wavelen)
+    post_value = posterior(params, obs_flux, obs_flux_unc, exp_distance, exp_Teff, Teff_unc, log_g, log_unc, metallicity, met_unc, filter_wavelen)
+    prior_list.append(prior_value)
+    likelihood_list.append(likelihood_value)
+    posterior_list.append(post_value)
+# %%
+plt.plot(temp_values, (prior_list))
+plt.title('Prior probability distribution')
+plt.xlabel('Temperature')
+plt.ylabel('Probability')
+plt.show()
+plt.plot(temp_values, (likelihood_list))
+plt.title('Likelihood probability distribution')
+plt.xlabel('Temperature')
+plt.ylabel('Probability')
+plt.show()
+plt.plot(temp_values, (posterior_list))
+plt.title('Posterior probability distribution')
+plt.xlabel('Temperature')
+plt.ylabel('Probability')
+plt.show()
 # %%
