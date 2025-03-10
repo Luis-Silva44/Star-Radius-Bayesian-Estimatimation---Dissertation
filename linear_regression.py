b1d5cefba6166adff3ec582ea447885fa35ad12f
Luis <luismms1999@hotmail.com>
2025-03-10 11:32:36 +0000

    Testing the priors

--- linear_regression.py
+++ linear_regression.py
@@ -31,12 +31,14 @@ def log_likelihood(theta, x, y, yerr):
     m, b = theta
     model = m * x + b
     sigma2 = yerr**2
-    return -0.5 * np.sum((y - model) ** 2 / sigma2)
+    return (-0.5 * np.sum((y - model) ** 2 / sigma2) + np.sum(np.log(2 * np.pi * sigma2))) 
 
 def log_prior(theta):
     m, b = theta
-    if -5.0 < m < 0.5 and 0.0 < b < 10.0:
-        return 0.0
+    m_prior = -0.5 * (m - 1.5)**2 / 0.005**2 
+    log_m_prior = -0.5 * ((m - 1.5) / 0.005) ** 2 - np.log(0.005 * np.sqrt(2 * np.pi))  # Added normalization
+    if -5.0 < m < 5.0 and 0.0 < b < 10.0:
+        return log_m_prior
     return -np.inf
 
 def log_probability(theta, x, y, yerr):
@@ -45,13 +47,13 @@ def log_probability(theta, x, y, yerr):
         return -np.inf
     return lp + log_likelihood(theta, x, y, yerr)
 
-pos = (-0.90, 4.3) + 1e-4 * np.random.randn(32, 2)
+pos = (1.2, 4.3) + 1e-4 * np.random.randn(32, 2)
 nwalkers, ndim = pos.shape
 
 sampler = emcee.EnsembleSampler(
     nwalkers, ndim, log_probability, args=(x, y, yerr)
 )
-sampler.run_mcmc(pos, 5000, progress=True)
+state = sampler.run_mcmc(pos, 500, progress=True)
 
 # %% 
 fig, axes = plt.subplots(2, figsize=(10, 7), sharex=True)
@@ -65,7 +67,7 @@ for i in range(ndim):
     ax.yaxis.set_label_coords(-0.1, 0.5)
 
 axes[-1].set_xlabel("step number")
-flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+flat_samples = sampler.get_chain(flat=True)
 
 fig = corner.corner(
     flat_samples, labels=labels, truths=[m_true, b_true])
@@ -88,4 +90,28 @@ for i in range(ndim):
     txt = txt.format(mcmc[1], q[0], q[1], labels[i])
     display(Math(txt))
 
+# %%
+
+m_values = np.linspace(-5,5,20)
+b_true = 4.294
+prior_list = []
+posterior_list = []
+likelihood_list = []
+
+for m in m_values:
+    theta = (m, b_true)
+    likelihood_list.append(log_likelihood(theta, x, y, yerr))
+    prior_list.append(log_prior(theta))
+    posterior_list.append(log_probability(theta, x, y, yerr))
+# %%
+print(likelihood_list)
+plt.plot(m_values, (prior_list))
+plt.xlim(-3,5)
+plt.show()
+plt.plot(m_values, (likelihood_list))
+plt.xlim(-3,5)
+plt.show()
+plt.plot(m_values, (posterior_list))
+plt.xlim(-3,5)
+plt.show()
 # %%
